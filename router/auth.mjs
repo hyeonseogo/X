5052c9f456b727676ead7721016aad18d6663c1a
고현서 <hyeonseogo01@gmail.com>
2025-04-24 14:46:13 +0900

    배열을 이용한 AUTH API 테스트

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,11 +1,16 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs";
 
 const router = express.Router();
 
 // 회원가입
+// POST
+// http://127.0.0.1:9090/auth/signup
+router.post("/signup", authController.signup);
 
 // 로그인
-
-// 로그인 유지
+// POST
+// http://127.0.0.1:9090/auth/login
+router.post("/login", authController.login);
 
 export default router;
